6ae8ce13be75b9df6268e2446038f1d194aa6822
Sushma Mangalapally <sushma.m812@gmail.com>
2025-07-16 00:36:03 -0700

    demo

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+// App.jsx
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState("");
+
+  const fetchMessages = async () => {
+    const res = await axios.get("http://localhost:8080/messages");
+    console.log(res)
+    setMessages(res.data);
+  };
+
+  const sendMessage = async () => {
+    await axios.post("http://localhost:8080/messages", { content: input });
+    setInput("");
+    fetchMessages();
+  };
+
+  useEffect(() => {
+    fetchMessages();
+  }, []);
+
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Message Board</h1>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Write a message"
+      />
+      <button onClick={sendMessage}>Send</button>
+      <ul>
+        {messages.map((msg, idx) => {
+          const mess = JSON.parse(msg);
+          return (
+          <li key={idx}>{mess.content}</li>
+          )
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
